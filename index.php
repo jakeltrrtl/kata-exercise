4283625af4b4f11c74f06ef7764568974e5836d1
Jake Richardson <jakerichardson13@googlemail.com>
2019-05-09 16:10:20 +0100

    Commit 1.0
    This is the first commit. The base layout of the project is defined using the Bootstrap framework.

---
+++ index.php
@@ -0,0 +1,71 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- BOOTSTRAP CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <!-- MAIN CSS -->
+    <link rel="stylesheet" href="../assets/css/main.css?<?php echo time(); ?>" type="text/css">
+
+    <!-- VARELA ROUND FONT -->
+    <link href="https://fonts.googleapis.com/css?family=Varela+Round" rel="stylesheet">
+
+    <!-- APP TITLE -->
+    <title>Bright HR</title>
+  </head>
+  <body>
+
+    <!-- HEADER -->
+    <header>
+      <h1>Files & Folders</h1>
+    </header>
+    <!-- END OF HEADER -->
+
+
+    <!-- FILTER SECTION -->
+    <section id="filter" class="white">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-2"></div>
+                <div class="col-lg-8">
+                    <div class="row">
+                        <div class="col-lg-4"></div>
+                        <div class="col-lg-4"></div>
+                        <div class="col-lg-4">
+                            <!-- TODO add search bar to search for file name -->
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-2"></div>
+            </div>
+        </div>
+    </section>
+    <!-- END OF SECTION -->
+
+    <!-- MAIN CONTENT SECTION -->
+    <section id="content" class="white">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-2"></div>
+                <div class="col-lg-8">
+                  <h2>To filter the table select the heading</h2>
+                    <!-- TODO add the xml table and add options to sort the headers --> 
+                </div>
+                <div class="col-lg-2"></div>
+            </div>
+        </div>
+    </section>
+    <!-- END OF SECTION -->
+
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
